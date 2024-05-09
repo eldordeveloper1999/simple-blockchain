e9f82ff07277afcb899e4da6e330dff6c731fbf8
eldordeveloper1999 <eldordeveloper1999@gmail.com>
2024-05-09 11:26:56 +0500

    add parts of blockchain

--- src/main/java/org/example/NoobChain.java
+++ src/main/java/org/example/NoobChain.java
@@ -1,8 +1,44 @@
 package org.example;
 
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
 public class NoobChain {
 
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+
     public static void main(String[] args) {
-        Block genesisBlock = new Block("Hi im the first block", "0");
+        // add our blocks to the blockchain Arraylist:
+        blockchain.add(new Block("Hi im the first block", "0"));
+        blockchain.add(new Block("Yo im the second block", blockchain.getLast().hash));
+        blockchain.add(new Block("Hey im the third block", blockchain.getLast().hash));
+        blockchain.add(new Block("Hell no im the fourth block", blockchain.getLast().hash));
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+
+    }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        //loop through blockchain to check up
+        for (int i = 0; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            //compare registered hash and calculated hash:
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
     }
 }
